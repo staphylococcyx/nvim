9ba4aeac7735f79d357d9a1a1d2e88bbdbc7db83
Anas Youssef El Mahdad <elmahdad.anasyoussef00@proton.me>
2025-06-12 20:38:11 +0100

    gruvbox-nvim

--- lua/staphylococcyx/lazy/plugins/gruvbox.lua
+++ lua/staphylococcyx/lazy/plugins/gruvbox.lua
@@ -1,8 +1,19 @@
 return {
-	'morhetz/gruvbox',
-	init = function()
-		vim.opt.background = 'dark'
-		vim.g.gruvbox_italic = 1
-		vim.cmd [[colorscheme gruvbox]]
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = true,
+	init = function ()
+		vim.opt.background = "dark" -- or "light" for light mode
+		vim.cmd([[colorscheme gruvbox]])
 	end,
+	opts = {},
 }
+
+-- return {
+-- 	'morhetz/gruvbox',
+-- 	init = function()
+-- 		vim.opt.background = 'dark'
+-- 		vim.g.gruvbox_italic = 1
+-- 		vim.cmd [[colorscheme gruvbox]]
+-- 	end,
+-- }
